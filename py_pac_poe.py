14f8b23dbec49858571d6160b591ce137bbf74b8
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-11 12:05:37 -0600

    print board

--- py_pac_poe.py
+++ py_pac_poe.py
@@ -1,15 +1,53 @@
 # Py-Pac-Poe
 
 # Global Variables
+state = {}
 
 # Welcome Message
 def welcome():
     print("----------------------")
     print("Let's play Py-Pac-Poe!")
     print("----------------------")
-    
+    print()
+
+# Display Board
+def print_board():
+    print( "     A   B   C ")
+    print()
+    print( f"1)   {state['board']['a1'] if state['board']['a1'] else ' '} | {state['board']['b1'] if state['board']['b1'] else ' '} | {state['board']['c1'] if state['board']['c1'] else ' '} ")
+    print( "    -----------")
+    print( f"1)   {state['board']['a2'] if state['board']['a2'] else ' '} | {state['board']['b2'] if state['board']['b2'] else ' '} | {state['board']['c2'] if state['board']['c2'] else ' '} ")
+    print( "    -----------")
+    print( f"1)   {state['board']['a3'] if state['board']['a3'] else ' '} | {state['board']['b3'] if state['board']['b3'] else ' '} | {state['board']['c3'] if state['board']['c3'] else ' '} ")
+    print()
+
+# Get Move
+def get_move():
+    pass
+
+# Get Winner
+def get_winner():
+    return None
+
 # Initialize Game
 def init_game():
+    # Initialize current state of the game
+    state['board'] = {
+        'a1': 'X', 'b1': None, 'c1': None,
+        'a2': None, 'b2': 'O', 'c2': None,
+        'a3': None, 'b3': None, 'c3': None
+    }
+    state['turn'] = 'X'
+
+    # Display welcome message
     welcome()
+
+    #while not get_winner():
+    # Display board
+    print_board()
+
+
+        # Get Move
+        # move = get_move().lower()
 
 init_game()
