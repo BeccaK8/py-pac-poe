6b267137918384c3eca5d12a69b856489173245b
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-11 11:01:47 -0600

    initial commit with welcome message

---
+++ py_pac_poe.py
@@ -0,0 +1,15 @@
+# Py-Pac-Poe
+
+# Global Variables
+
+# Welcome Message
+def welcome():
+    print("----------------------")
+    print("Let's play Py-Pac-Poe!")
+    print("----------------------")
+    
+# Initialize Game
+def init_game():
+    welcome()
+
+init_game()
